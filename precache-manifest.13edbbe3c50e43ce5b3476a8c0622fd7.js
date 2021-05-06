202d5c346489b20c2a55b6c3901273bbc2c4d3c9
Alagie-Drammeh <unrulyfitruly@gmail.com>
2021-05-06 10:14:19 +0200

    delpoying projec

---
+++ precache-manifest.13edbbe3c50e43ce5b3476a8c0622fd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5027565d9b907cc31ba15945107119b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5978bd479f33bf329f78",
+    "url": "./static/css/main.fbb17e3a.chunk.css"
+  },
+  {
+    "revision": "cf96070990999b9e4a85",
+    "url": "./static/js/2.6bdcfe48.chunk.js"
+  },
+  {
+    "revision": "5978bd479f33bf329f78",
+    "url": "./static/js/main.02714b42.chunk.js"
+  },
+  {
+    "revision": "baab4f6995ba25947e6f",
+    "url": "./static/js/runtime-main.f5f70450.js"
+  }
+]);
